41d46ef48b7f64aa0e4e920864b7137685de6ab1
Luke John <luke.john@osmahi.com>
2013-09-29 19:30:03 +1000

    updated controller

--- controller/app.js
+++ controller/app.js
@@ -1,16 +1,22 @@
+'strict'
 
 /**
  * Module dependencies.
  */
 
 var express = require('express');
-var routes = require('./routes');
+var adminroutes = require('./routes/admin');
 var http = require('http');
 var path = require('path');
 var request = require('request');
 
+// datastore connection
+
 var Datastore = require('nedb')
   , db = new Datastore({ filename: 'storage/stations.db' , autoload: true});
+
+// config
+var config = require('../config.json')
 
 var app = express();
 
@@ -32,9 +38,11 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+app.locals(config.event)
+
 var storeStation = function(settings, ip) {
   var document = {
-    ip: ip,
+    ip: ip || null,
     settings: settings
   }
   db.insert(document, function (err, newDoc) {
@@ -60,36 +68,34 @@ var Station = function(mac, roles, room, nickname) {
 }
 
 var getStation = function(mac, ip) {
-  removeIp(req.ip)
+  removeIp(ip)
   request('http://' +ip +':3000/settings/'+mac, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      storeStation(response.body)
-    }
-    if (!error && response.statusCode == 204) {
-      var station = new Station(mac)
-      request('http://' +ip +':3000/settings', { 
-        method: 'POST',
-        json: station
-      }, function(error, response, data) {
-        if (!error && response.statusCode == 201) {
-          storeStation(station)
-        }
-      })
-    }
-    if (!error && response.statusCode == 400) {
-      getStation(response.body.mac, ip)
+    if (!error) {
+      console.log(response.statusCode)
+      if (response.statusCode == 200) {
+        console.log(response.body)
+        storeStation(response.body, ip)
+      }
+      if (response.statusCode == 204) {
+        
+      }
+      if (response.statusCode == 400) {
+        getStation(response.body.mac, ip)
+      }
     }
   })
 }
+
+app.get('/admin', adminroutes.dashboard)
 
 app.get('/station/:mac', function(req, res) {
   db.find({ 'settings.mac': req.params.mac }, function (err, docs) {
     if (docs) {
       if (!docs.length) {
-        res.send(204, {
-          status: 'unknown'
-        })
         getStation(req.params.mac, req.ip)
+        res.send(204, {
+          "status": 'unknown'
+        })
       }
       if (docs.length) {
         var doc = docs[0]
@@ -103,8 +109,29 @@ app.get('/station/:mac', function(req, res) {
   });
 });
 
-app.get('/admin', function(request, response) {
-  response.render('admin/index', {title: 'eventstreamr'})
+app.get('/api/stations', function(req, res) {
+  db.find({}, function (err, docs) {
+    if (err) {
+      res.send(500, err)
+    }
+    if (docs) {
+      res.send(docs)
+    }
+  });
+})
+
+app.post('/api/station', function(req, res) {
+  var station = new Station(req.body.mac.replace(/:\s*/g, "-"), req.body.roles, req.body.room, req.body.nickname)
+  storeStation(station)
+  res.send(true)
+})
+
+app.del('/api/station/:mac', function(req, res) {
+  db.remove({'_id': req.params.mac}, {}, function(err, removed) {
+    if (removed) {
+      res.send(true)
+    }
+  })
 })
 
 http.createServer(app).listen(app.get('port'), function(){
