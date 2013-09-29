b7ec0cb9021eeb027917404168d4f535b6c795d9
Luke John <luke.john@osmahi.com>
2013-09-29 16:00:30 +1000

    controller base api set

--- controller/app.js
+++ controller/app.js
@@ -7,11 +7,17 @@ var express = require('express');
 var routes = require('./routes');
 var http = require('http');
 var path = require('path');
+var request = require('request');
+
+var Datastore = require('nedb')
+  , db = new Datastore({ filename: 'storage/stations.db' , autoload: true});
 
 var app = express();
 
+app.enable('trust proxy');
+
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', 5001);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
@@ -26,48 +32,81 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});/**
- * Module dependencies.
- */
-
-var express = require('express');
-var http = require('http');
-var request = require('request');
-var getmac =  require('getmac').getMac;
-var fs = require('fs');
-
-// station data
-var station = {};
-
-var app = express();
-
-// all environments
-app.set('port', 5001);
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+var storeStation = function(settings, ip) {
+  var document = {
+    ip: ip,
+    settings: settings
+  }
+  db.insert(document, function (err, newDoc) {
+    
+  })
 }
 
-app.get('/station/:id', function(req, res) {
-  res.send('success')
+var removeIp = function(ip) {
+  db.update({ ip: ip }, { $unset: { ip: true } }, {}, function () {});
+}
+
+var updateIp = function(mac, ip) {
+  db.update({ "setings.mac": mac }, { $set: { ip: ip } }, {}, function () {});
+}
+
+var Station = function(mac, roles, room, nickname) {
+  return {
+    mac: mac,
+    roles: roles || [],
+    room: room || null,
+    nickname: nickname || null
+  }
+}
+
+var getStation = function(mac, ip) {
+  removeIp(req.ip)
+  request('http://' +ip +':3000/settings/'+mac, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      storeStation(response.body)
+    }
+    if (!error && response.statusCode == 204) {
+      var station = new Station(mac)
+      request('http://' +ip +':3000/settings', { 
+        method: 'POST',
+        json: station
+      }, function(error, response, data) {
+        if (!error && response.statusCode == 201) {
+          storeStation(station)
+        }
+      })
+    }
+    if (!error && response.statusCode == 400) {
+      getStation(response.body.mac, ip)
+    }
+  })
+}
+
+app.get('/station/:mac', function(req, res) {
+  db.find({ 'settings.mac': req.params.mac }, function (err, docs) {
+    if (docs) {
+      if (!docs.length) {
+        res.send(204, {
+          status: 'unknown'
+        })
+        getStation(req.params.mac, req.ip)
+      }
+      if (docs.length) {
+        var doc = docs[0]
+        if (doc.ip !== req.ip) {
+          removeIp(req.ip)
+          updateIp(req.params.mac, req.ip)
+        }
+        res.send(200, docs[0])
+      }
+    }
+  });
 });
+
+app.get('/admin', function(request, response) {
+  response.render('admin/index', {title: 'eventstreamr'})
+})
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Eventstreamr controller listening on port ' + app.get('port'));
 });
-
-request('controller.eventstreamr:5001/config/'+ station.id, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-  }
-  else {
-  }
-})
