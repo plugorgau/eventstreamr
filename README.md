258f69cd040f3d4f67953e25c7d67c379fc6efb5
Leon Wright <techman83@gmail.com>
2013-11-03 10:14:07 +0800

    Discovery of v4l devices completed

--- README.md
+++ README.md
@@ -6,25 +6,4 @@ Single and multi room audio visual stream management.
 Station Script Requirements
 ===========================
 Apt Packages:
-libdancer-perl libipc-shareable-perl libproc-daemon-perl libipc-shareable-perl libjson-perl libconfig-json-perl libdevice-usb-perl
-
-USB Permissions
-===============
-copy ./station/etc/udev/rules.d/99-usb-perms.rules to /etc/udev/rules.d/99-usb-perms.rules
-
-Add the usb group
-sudo groupadd --system usb
-
-Add the group to the user
-sudo usermod -G -a usb username
-
-Trigger udev reload
-sudo udevadm trigger reload
-
-You can find the system rules here:
-/lib/udev/rules.d/50-udev-default.rules
-
-Our rule appends
-, GROUP="usb"
-
-To the system udev rule. Putting it in /etc/udev/rules.d with a higher number overrides the system default. 
+libdancer-perl libipc-shareable-perl libproc-daemon-perl libipc-shareable-perl libjson-perl libconfig-json-perl
--- station/lib/EventStreamr/Devices.pm
+++ station/lib/EventStreamr/Devices.pm
@@ -1,26 +1,78 @@
 package EventStreamr::Devices;
 use Moo; # libmoo-perl
-use Device::USB; # libdevice-usb-perl
+use Cwd 'realpath';
+
 use Data::Dumper;
 
 sub list {
   my $self = shift;
-  @{$self->{usb_devices}} = usb_devices();
+  @{$self->{v4l_devices}} = v4l_devices();
   return;
 }
 
-sub usb_devices {
-  my $usb = Device::USB->new();
-  my $devices;
-  my @devices = $usb->list_devices();
-  foreach my $device (@devices) {
-    my $dev = $usb->find_device( $device->{descriptor}{idVendor}, $device->{descriptor}{idProduct} );
-    #$dev->open();
-    print "Manufactured by ", $dev->manufacturer(), "\n",
-      " Product: ", $dev->product(), "\n";
-    #print Dumper($dev);
+sub v4l_devices {
+  my @v4ldevices = </dev/video*>;
+  my $v4l_devices;
+  foreach my $device (@v4ldevices) {
+    $device =~ m/\/dev\/(?<index>.+)/;
+    my $index = $+{index};
+    $v4l_devices->{$index}{path} = $device;
+    $v4l_devices->{$index}{name} = get_v4l_name($index);
   }
-  return $devices;
+  return $v4l_devices;
+}
+
+sub get_v4l_name {
+  my ($device) = @_;
+  my $name;
+
+  # Find USB
+  my $index = $+{index};
+  my @usbs = </dev/v4l/by-id/*>;
+  foreach my $usb (@usbs) { # suffers from Big0 notation, but should only be a limited number of devices
+    if ( realpath($usb) =~ /$index/ ) {
+      $usb =~ m/\/dev\/v4l\/by-id\/usb-(?<name> .+)-video-index\d/ix;
+      $name = $+{name};
+
+      # Some older devices don't present a name in the path but an ID
+      if ( $name =~ /^[^+s]{4}_[^+s]{4}$/ ) {
+        $name = name_lsusb($name);
+      } else {
+        $name =~ s/_/\ /g;
+      }
+      last;
+    }
+  }
+  # Find PCI
+  unless ($name) {
+    my @pcis = </dev/v4l/by-path/*>;
+    foreach my $pci (@pcis) {
+      if ( realpath($pci) =~ /$index/ ) {
+        $pci =~ m/pci-[^+s]{4}:(?<pciid>..:..\..)-video-index\d/ix;
+        $name = name_lspci($+{pciid});
+        last;
+      }
+    }
+  }
+
+  return $name;
+}
+
+sub name_lsusb {
+  my ($name) = @_;
+  $name =~ m/^(?<vid> [^+s]{4})_(?<did> [^+s]{4})$/ix;
+  $name = `lsusb | grep \"$+{vid}:$+{did}\"`;
+  $name =~ m/^Bus.\d+.Device.\d+:.ID.[^+s]{4}:[^+s]{4}.(?<name>.+)/ix;
+  $name = $+{name};
+  return $name;
+}
+
+sub name_lspci {
+  my ($name) = @_;
+  $name = `lspci | grep \"$name\"`;
+  $name =~ m/..:..\...(?<name>.+)/ix;
+  $name = $+{name};
+  return $name;
 }
 
 1;
