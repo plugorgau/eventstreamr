f7a4bb934cb7201b8354356f84d396fc59e05ef3
Leon Wright <leon.wright@imdexlimited.com>
2013-11-11 17:40:24 +0800

    added get pid method

--- station/lib/EventStreamr/Utils.pm
+++ station/lib/EventStreamr/Utils.pm
@@ -1,15 +1,20 @@
 package EventStreamr::Utils;
 use Moo;
-use IO::Socket::INET;
+use IO::Socket::INET; # System Class
+use Proc::ProcessTable; # libproc-processtable-perl
+use feature 'switch';
+use Data::Dumper;
 
 sub test {
   my $self = shift;
   $self->{portstate} = port("localhost","1234");
+  $self->{get_pid_command} = get_pid_command("video0","ffmpeg -f video4linux2 -s vga -r 25 -i /dev/video0 -target pal-dv - | dvsource-file /dev/stdin -h localhost -p 1234","v4l");
   return;
 }
 
 sub port {
-  my ($self,$host,$port) = @_;
+  my $self = shift;
+  my ($host,$port) = @_;
   my $state;
   my $sock = new IO::Socket::INET ( PeerAddr => $host,
                                     PeerPort => $port,
@@ -24,4 +29,26 @@ sub port {
   return $state;
 }
 
+sub get_pid_command {
+  my $self = shift;
+  my ($id,$command,$type) = @_;
+  my $regex;
+  my $return;
+  
+  given ($type) {
+    when ("v4l")  { $regex = "ffmpeg.+\\/dev\\/$id.*"; }
+    default       { $regex = $command }
+  }
+
+  my $pt = Proc::ProcessTable->new;
+  my @procs = grep { $_->cmndline =~ /^$regex/ } @{ $pt->table };
+  if (@procs) {
+    $return->{pid} = $procs[0]->pid;
+    $return->{running} = 1;
+  } else {
+    $return->{running} = 0;
+  }
+  
+  return $return;
+}
 1
