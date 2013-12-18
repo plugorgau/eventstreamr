afff1120fa51a13d29854ff6ea0aada65ab6c798
Leon Wright <techman83@gmail.com>
2013-12-18 20:45:38 +0800

    likely that testing will occur locally

--- controller/test.js
+++ controller/test.js
@@ -1,6 +1,6 @@
 var request = require('request');
 
-request('http://10.4.1.196:5001/station/00-21-5c-02-02-27', { 
+request('http://127.0.0.1:5001/api/station/00-21-5c-02-02-27', { 
   method: 'GET'
 }, function(error, response, data) {
   console.log('test', error || response)
