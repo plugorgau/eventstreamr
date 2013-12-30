7a6fdd068acbe577d5bef7fb2b8e4db21a100d19
AV person <luke.john@osmahi.com>
2013-12-30 19:18:18 +1100

    updated ui client to use sane bindings and readded missing frontend js

---
+++ controller/public/javascripts/client.js
@@ -0,0 +1,58 @@
+ko.bindingProvider.instance = new StringInterpolatingBindingProvider();
+
+var viewModel = {
+  stations: ko.mapping.fromJS([])
+}
+ko.applyBindings(viewModel);
+
+var socket = io.connect('//:5001')
+
+
+$.get( "/api/stations", function( data ) {
+})
+  .done(function(data) {
+    ko.mapping.fromJS(data, viewModel.stations)
+    socket.on('change', function (data) {
+      console.log(data);
+      if (data.type == 'remove') {
+        viewModel.stations.remove(function(item) { 
+          return item._id() == data.content
+        })
+      }
+      if (data.type == 'insert') {
+        viewModel.stations.push(ko.mapping.fromJS(data.content))
+      }
+    });
+  })
+
+
+
+
+var removeStation = function(data, event) {
+  $.ajax({
+    url: "/api/station/"+ data._id(),
+    type: 'DELETE'
+  })
+    .done(function(data) {
+      console.log( "removed station", JSON.stringify(data) );
+    })
+}
+
+function submitStation(data, event) {
+  var $form = $(event.currentTarget).parents('#add-station');
+
+  $.ajax({
+    type: "POST",
+    url: "/api/station",
+    data: data,
+  })
+    .done(function() {
+      $form.collapse('hide')
+    })
+}   
+
+var macRewrite = function(macaddress) {
+  if (!macaddress) { return false }
+  return macaddress.replace(/-\s*/g, ":")
+}
+
