35d0dc50e77e40e2b2fd3f2e65b88e0f18e94439
Leon Wright <techman83@gmail.com>
2014-01-02 23:09:25 +0800

    client js changes to support interface requirements

--- controller/public/javascripts/client.js
+++ controller/public/javascripts/client.js
@@ -7,6 +7,14 @@ var availableRoles = [
   {value: 'stream', text: 'stream'}
 ]
 
+var availableDevices = function(devices) {
+  var dataArray = $.map(devices,function(v){
+    return {"value":v.id, "text": v.name};
+  });
+  console.log(dataArray)
+  return dataArray
+}
+
 var roleDisplay = function(value, sourceData) {
    var selectedRoles = "",
        checked = $.fn.editableutils.itemsByValue(value, sourceData);
@@ -18,6 +26,31 @@ var roleDisplay = function(value, sourceData) {
      $(this).html('<ul class="list-inline">' + selectedRoles + '</ul>');
    } else {
      $(this).empty(); 
+   }
+}
+
+var deviceDisplay = function(value, sourceData) {
+  var selectedRoles = "",
+       checked = $.fn.editableutils.itemsByValue(value, sourceData);
+       
+  if(checked.length) {
+    $.each(checked, function(i, v) { 
+      selectedRoles += '<div class="col-sm-6">' +
+        '<div class="panel panel-default">' +
+          '<div class="panel-heading">' +
+            '<div class="control-actions btn-group">' +
+              '<a class="btn btn-success btn-xs"> Start </a><a class="btn btn-warning btn-xs"> Restart</a><a class="btn btn-danger btn-xs"> Stop</a>' +
+            '</div>' +
+          '</div>' +
+          '<div class="panel-body">' +
+            $.fn.editableutils.escape(v.text) +
+          '</div>' +
+        '</div>' +
+      '</div>';
+     });
+     $(this).html('<small><div class="row">' + selectedRoles + '</div></small>');
+   } else {
+     $(this).html('<small><button class="btn btn-default btn-xs">Add Device</button>'); 
    }
 }
 
@@ -62,19 +95,40 @@ $.get( "/api/stations", function( data ) {
         });
        
         if (match) {
-          // need to actually do somethings..
+          viewModel.stations.splice(viewModel.stations.indexOf(match),1,ko.mapping.fromJS(data.content));
         }
       }
     });
   })
 
-
-
+var actionDevicePost = function(data) {
+  $.ajax({
+    url: data.station_ip + '/command/' + data.action,
+    type: 'POST',
+    data: {
+      id: data.device_id,
+    }
+  })
+}
 
 var removeStation = function(data, event) {
   $.ajax({
     url: "/api/station/"+ data.settings.macaddress(),
     type: 'DELETE'
+  })
+    .done(function(data) {
+      console.log( "removed station", JSON.stringify(data) );
+    })
+}
+
+var removeStationRoom = function(data, event) {
+  $.ajax({
+    url: "/api/stations/"+ data.settings.macaddress()  + '/partial',
+    type: 'POST',
+    data: {
+      key: 'settings.room',
+      value: ''
+    }
   })
     .done(function(data) {
       console.log( "removed station", JSON.stringify(data) );
