2cb8a439ff8df96c971991ebe9d214ea01149e95
Leon Wright <techman83@gmail.com>
2014-01-04 01:16:06 +0800

    various frontend implementations

--- controller/public/javascripts/client.js
+++ controller/public/javascripts/client.js
@@ -7,11 +7,21 @@ var availableRoles = [
   {value: 'stream', text: 'stream'}
 ]
 
-var availableDevices = function(devices) {
+var availableDevices = function(devices,configured) {
   var dataArray = $.map(devices,function(v){
-    return {"value":v.id, "text": v.name};
+    var availableDevice = {};
+    availableDevice.name = v.name;
+    availableDevice.id = v.id;
+    availableDevice.type = v.type;
+    for(var i in configured()) {
+      var id = configured()[i].id();
+      var test = availableDevice.id();
+      if (id == test) {
+        return;
+      }
+    }
+    return availableDevice;
   });
-  console.log(dataArray)
   return dataArray
 }
 
@@ -26,31 +36,6 @@ var roleDisplay = function(value, sourceData) {
      $(this).html('<ul class="list-inline">' + selectedRoles + '</ul>');
    } else {
      $(this).empty(); 
-   }
-}
-
-var deviceDisplay = function(value, sourceData) {
-  var selectedRoles = "",
-       checked = $.fn.editableutils.itemsByValue(value, sourceData);
-       
-  if(checked.length) {
-    $.each(checked, function(i, v) { 
-      selectedRoles += '<div class="col-sm-6">' +
-        '<div class="panel panel-default">' +
-          '<div class="panel-heading">' +
-            '<div class="control-actions btn-group">' +
-              '<a class="btn btn-success btn-xs"> Start </a><a class="btn btn-warning btn-xs"> Restart</a><a class="btn btn-danger btn-xs"> Stop</a>' +
-            '</div>' +
-          '</div>' +
-          '<div class="panel-body">' +
-            $.fn.editableutils.escape(v.text) +
-          '</div>' +
-        '</div>' +
-      '</div>';
-     });
-     $(this).html('<small><div class="row">' + selectedRoles + '</div></small>');
-   } else {
-     $(this).html('<small><button class="btn btn-default btn-xs">Add Device</button>'); 
    }
 }
 
@@ -93,31 +78,93 @@ $.get( "/api/stations", function( data ) {
         var match = ko.utils.arrayFirst(viewModel.stations(), function(item) {
           return data.content._id === item._id();
         });
-       
         if (match) {
           viewModel.stations.splice(viewModel.stations.indexOf(match),1,ko.mapping.fromJS(data.content));
         }
       }
+      if (data.type == 'notify') {
+      }
+
     });
   })
 
-var actionStation = function(ip, action) {
-  var post = new Object();
-  post.station_ip = ip;
-  post.id = "Station";
-  post.action = action + "/all";
-  actionDevicePost(post);
-
+var actionStationManagers = function(roomId, action) {
+  ko.utils.arrayForEach(viewModel.stations(), function(station) {
+    if (station.settings.room() === roomId) {
+      var post = new Object();
+      post.station_macaddress = station.settings.macaddress();
+      post.id = "Station";
+      post.command_url = "manager";
+      post.action = action;
+      actionStationPost(post);
+    }
+  })
 }
 
-var actionDevicePost = function(post) {
+var actionStations = function(roomId, action) {
+  ko.utils.arrayForEach(viewModel.stations(), function(station) {
+    if (station.settings.room() === roomId) {
+      var post = new Object();
+      post.station_macaddress = station.settings.macaddress();
+      post.id = "all";
+      post.command_url = "command";
+      post.action = action;
+      actionStationPost(post);
+    }
+  })
+}
+
+var actionStationManager = function(macaddress, action) {
+  var post = new Object();
+  post.station_macaddress = macaddress;
+  post.id = "Station";
+  post.command_url = "manager";
+  post.action = action;
+  actionStationPost(post);
+}
+
+var availableDeviceClick = function (checked, configured, macaddress) {
+  var value = {};
+  value.id = checked.id();
+  value.type = checked.type();
+  value.name = checked.name();
+  configured.push(value);
+  
+  var post = {};
+  post.key = "settings.devices";
+  post.value = configured();
+  
+  $.ajax({
+    url: '/api/stations/' + macaddress() + '/partial',
+    type: 'POST',
+    data: post 
+  })
+};
+
+$("body").on("click", ".actionOnclick", function (e) {
+  var post = new Object();
+  post.station_macaddress = $(e.currentTarget).closest("[data-macaddress]").data('macaddress');
+  post.id = $(e.currentTarget).data('id');
+  post.command_url = "command";
+  post.action = $(e.currentTarget).data('action');
+  actionStationPost(post);
+});
+
+var actionDevice = function(macaddress, device, action) {
+  var post = new Object();
+  post.station_macaddress = macaddress;
+  post.id = device;
+  post.command_url = "command";
+  post.action = action;
+  actionStationPost(post);
+}
+
+var actionStationPost = function(post) {
   console.log(post)
   $.ajax({
-    url: 'http://' + post.station_ip + ':3000/command/' + post.action,
+    url: '/api/station/' + post.station_macaddress + '/action',
     type: 'POST',
-    data: {
-      id: post.device_id,
-    }
+    data: post 
   })
 }
 
