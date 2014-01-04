fb2fb60900196134a0e00f5c09c4e94c8fd4a958
Jason Nicholls <jason@permietech.com.au>
2014-01-04 18:05:58 +0800

    shift left

--- baseimage/update-wallpaper.sh
+++ baseimage/update-wallpaper.sh
@@ -6,9 +6,9 @@
 echo "- updating background image"
 convert $IMAGE/pictures/av-background-orig.png \
 	-fill "#333333" -font Ubuntu-Mono-Bold -pointsize 24 \
-	-draw "text 1600,795 'hostname: ${HOSTNAME}" \
-	-draw "text 1600,820 ' IP addr: ${IPADDR}" \
-	-draw "text 1600,845 'MAC addr: ${MACADDR}" \
+	-draw "text 1590,795 'hostname: ${HOSTNAME}" \
+	-draw "text 1590,820 ' IP addr: ${IPADDR}" \
+	-draw "text 1590,845 'MAC addr: ${MACADDR}" \
 	~/Pictures/av-background.png
 
 exit 0
