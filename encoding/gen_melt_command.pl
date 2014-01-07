cdb373e656809b4616cbcff3765b2f93be9ad3ec
Leon Wright <techman83@gmail.com>
2014-01-07 15:33:48 +0800

    intro title generation complete

--- encoding/gen_melt_command.pl
+++ encoding/gen_melt_command.pl
@@ -6,12 +6,24 @@ use Getopt::Long;
 use HTTP::Tiny; # libhttp-tiny-perl
 use Data::Dumper;
 use DateTime::Format::Strptime;
+use Cache::FileCache;
+use File::Basename 'basename';
+use Image::Magick;
 
 # +- Range in seconds 
-my $range = 1500;
+our $self;
+$self->{range} = 1500;
+$self->{default_expires} = 300;
+$self->{cache_root} = '/tmp/schedule/';
+
+# change this to make path if dealing with sub directories.
+if (! -d $self->{cache_root}) {
+  mkdir $self->{cache_root};
+}
 
 #my $getopts_rc = GetOptions(
-#    "start-cut"        => \$start_cut,
+#    "start-cut"     => \$start_cut,
+#    "files"         => \@dvfiles,
 #    "end_cut"       => \$end_cut,
 #
 #    "help|?"        => \&print_usage,
@@ -33,21 +45,16 @@ my $range = 1500;
 
 # HTTP
 our $http = HTTP::Tiny->new(timeout => 15);
-our $self;
 
-my $response =  $http->post("https://lca2014.linux.org.au/programme/schedule/json");
-if ($response->{status} == 200 ) {
-  #print Dumper($response);
-  $self->{schedule} = from_json($response->{content});
-} else {
-  say "Schedule data not available";
-  say "$response->{status}";
-  say "$response->{content}";
-}
+my $test = '/tmp/2014-01-06_13-40-26.dv';
+$test = basename($test);
+$test =~ s/\.dv$//;
 
-my $test = '2014-01-06_13-40-26';
-#my $test =~ s/\.dv//;
+$self->{firstdv} = $test;
 
+retrieve_schedule();
+
+# Get the list of venues
 my @venues;
 my $count = 0;
 foreach my $key (keys %{$self->{schedule}}) {
@@ -71,13 +78,14 @@ my $zooparse = DateTime::Format::Strptime->new(
   on_error => 'croak',
 );
 
-my $starttime = $dvparse->parse_datetime($test);
+my $starttime = $dvparse->parse_datetime($self->{firstdv});
 
+# Find Closest schedules
 $count = 0;
 foreach my $presentation (@venue) {
   my $time = $zooparse->parse_datetime($presentation->{start});
   my $diff = $starttime->epoch - $time->epoch;
-  if ($diff <= 1500 &&  $diff >= -1500) {
+  if ($diff <= $self->{range} &&  $diff >= -$self->{range}) {
     say "$count) $presentation->{title}";
   }
   $count++;
@@ -85,6 +93,7 @@ foreach my $presentation (@venue) {
 
 $count--;
 
+# Ask for Title info
 my $talk = &Prompt("Select the matching talk: 0 - $count");
 say "";
 
@@ -94,15 +103,15 @@ my $presenters = @venue[$talk]->{presenters};
 $title = &Prompt("Alter title:", "$title");
 $presenters = &Prompt("Alter Prestenters", "$presenters");
 
-my $text;
-
 if ($presenters eq 'n') {
-  $text = "+$title.txt";
+  $self->{title_text} = "+$title.txt";
 } else {
-  $text = " -filter watermark:\"+$title~$presenters.txt\"";
+  $self->{title_text} = "$title\n$presenters";
 }
 
-say $text;
+# Create titles
+$self->{output_file} = "/tmp/@venue[$talk]->{schedule_id}-title.png";
+create_title();
 
 sub Prompt { # inspired from here: http://alvinalexander.com/perl/edu/articles/pl010005
   my ($question,$default) = @_;
@@ -121,5 +130,40 @@ sub Prompt { # inspired from here: http://alvinalexander.com/perl/edu/articles/p
   } else {
     return $_;
   }
+}
+
+sub create_title {
+  my $im;
+  $im = new Image::Magick;
+  
+  my $text = 'Works like magick!\nthis is a new line fffff';
+  $im->Set( size => '768x200' );
+  $im->ReadImage('/tmp/blank_title.png');
+  
+  my $label=Image::Magick->new(size=>"700x200");
+  $label->Set(gravity => "Center", font => '/usr/share/fonts/truetype/ubuntu-font-family/Ubuntu-B.ttf', background => 'none');
+  $label->Read("label:$self->{title_text}");
+  $im->Composite(image => $label, gravity => 'Center');
+  $im->Write($self->{output_file});
+}
+
+sub retrieve_schedule {
+  my $cache = new Cache::FileCache( {namespace  => 'schedule_cache', default_expires_in => $self->{default_expires}, cache_root => $self->{cache_root} } );
+  $self->{raw_json} = $cache->get( 'schedule' );
+  $self->{cache} = 'Cached';
+  if ( not defined $self->{raw_json} ) {
+    my $response =  $http->get("https://lca2014.linux.org.au/programme/schedule/json");
+    if ($response->{status} != 200 ) {
+      say "Schedule data not available";
+      say "$response->{status}";
+      say "$response->{content}";
+      exit 0;
+    }
+    $self->{raw_json} = $response->{content};
+    $cache->set( 'schedule', $self->{raw_json} );
+    $self->{cache} = 'Fresh';
+  }
+  $self->{schedule} = from_json($self->{raw_json});
+  return;
 }
 
